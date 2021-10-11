69444ba40e47b700dbf66dbc8d59fce5261a1bf4
pablo michelini <pab.michelini@gmail.com>
2021-10-11 16:38:33 +0200

    capture id-elements

---
+++ app.js
@@ -0,0 +1,4 @@
+const productName = document.querySelector("#productName");
+const productPrice = document.querySelector("#productPrice");
+const productSave = document.querySelector("#button-save");
+const productCancel = document.querySelector("#button-clear");
