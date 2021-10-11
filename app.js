2b65677b4ba735648081937fb77d5e477ab261bd
pablo michelini <pab.michelini@gmail.com>
2021-10-11 17:06:54 +0200

    create new product card done

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const productName = document.querySelector("#productName");
 const productPrice = document.querySelector("#productPrice");
 const buttonSave = document.querySelector("#button-save");
 const buttonCancel = document.querySelector("#button-clear");
+const productList = document.querySelector("#product-list");
+
+const createNewProduct = (name, price) => {
+  const ionCard = document.createElement('ion-card');
+  const newProdcutItem = document.createElement('ion-card-content');
+  newProdcutItem.textContent = name + ': $' + price;
+  ionCard.appendChild(newProdcutItem);
+  productList.appendChild(ionCard);
+}
 
 buttonSave.addEventListener('click', () => {
   const name = productName.value;
   const price = productPrice.value;
-  console.log(name, price)
+  createNewProduct(name, price)
 })
