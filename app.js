9f8a356598981fd03e1b6ff9059ccc990210cbb9
pablo michelini <pab.michelini@gmail.com>
2021-10-11 16:44:48 +0200

    capture value-inputs in console

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const productName = document.querySelector("#productName");
 const productPrice = document.querySelector("#productPrice");
-const productSave = document.querySelector("#button-save");
-const productCancel = document.querySelector("#button-clear");
+const buttonSave = document.querySelector("#button-save");
+const buttonCancel = document.querySelector("#button-clear");
+
+buttonSave.addEventListener('click', () => {
+  const name = productName.value;
+  const price = productPrice.value;
+  console.log(name, price)
+})
